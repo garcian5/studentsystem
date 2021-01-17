d790ddee4c8a974ea0f6603d0be736e02f12087d
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-01-17 00:00:06 -0700

    created studentdirectory.js component

---
+++ src/components/StudentDirectory.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class StudentDirectory extends Component {
+  render() {
+    return (
+      <div>
+        <button>Back</button>
+        <button>Home</button>
+        <h1>ICS Student Directory</h1>
+        <h6>Name List</h6>
+        {/*list of names */}
+      </div>
+    )
+  }
+}
