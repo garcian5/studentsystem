9143ca92e8edb8dd37acd7663aadd58caddfc238
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-01-16 22:02:21 -0700

    initial admin & password handling in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,12 +8,54 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      admins: Admins
+      admins: Admins,
+      adminID: '',
+      adminPassword: ''
     }
   }
 
-  logIn = () => {
-    console.log('login clicked!')
+  inputChange = (event) => {
+    // these variables will be accessed through the event parameter
+    const { name, value } = event.target;
+    // set state variable to its entered value
+    this.setState({
+      [name]: value
+    })
+  }
+
+  logIn = (event) => {
+    // prevents automatic refresh after submit
+    event.preventDefault();
+
+    // a flag to check if id exists in the database (assuming there is no duplicate ids); false by default
+    let idExists = false;
+    let matchingAdmin = {}; // temp variable to hold whatever matching admin is on the database
+
+    // iterate through the list of admins and find if it exists in the database
+    for (const admin of this.state.admins) {      
+      // if it exists, set flag to true and mutate variable for admin and exit the loop
+      if (admin.id === this.state.adminID) {
+        idExists = true;
+        matchingAdmin = admin;
+        break;
+      } else {
+        // if id doesn't exist, continue the loop
+        continue;
+      }
+    }
+
+    // a switch case statement to check if password matches given the event that the id exists or it sends an error message if id doesn't exist
+    switch (idExists) {
+      case true:
+        if (matchingAdmin.password === this.state.adminPassword) {
+          console.log('id and password matches!');
+        } else {
+          console.log('id matches but password incorrect');
+        }
+        break;
+      default:
+        console.log('id does not exist!')
+    }
   }
 
   render() {
@@ -22,10 +64,20 @@ export default class Home extends Component {
       <div>
         <form onSubmit={this.logIn}>
           <label>Admin ID Number:
-            <input type="text" name=""/>
+            <input 
+              type="text" 
+              name="adminID" 
+              value={this.state.adminID}
+              onChange={this.inputChange}
+            />
           </label><br />
           <label>Password:
-            <input type="password" name=""/>
+            <input 
+              type="password" 
+              name="adminPassword"
+              value={this.state.adminPassword}
+              onChange={this.inputChange}
+            />
           </label><br />
           <button>Log In</button>
         </form>
--- src/data/admins.js
+++ src/data/admins.js
@@ -2,8 +2,8 @@
  * Admins dataset
  */
 const admins = [
-  { name: 'admin1', id: 1212, password: 12345 },
-  { name: 'admin2', id: 23232, password: 67890 }
+  { name: 'admin1', id: '1212', password: '12345' },
+  { name: 'admin2', id: '23232', password: '67890' }
 ];
 
 export default admins;
