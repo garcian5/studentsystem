f9c1115f84c9f9f957c8ad162c11a1846276b29e
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-01-17 21:35:56 -0700

    added back button to studentdirectory.js

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 export default class StudentDirectory extends Component {
+  // go back to departments page
+  backBtnClicked = () => {
+    this.props.history.push('/departments', this.props.history.location.state);
+  }
   render() {
     return (
       <div>
-        <button>Back</button>
+        <button onClick={this.backBtnClicked}>Back</button>
         <button>Home</button>
         <h1>ICS Student Directory</h1>
         <h6>Name List</h6>
