11471fd6f6057bb8136e5b35be17e54b2e187aac
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-01-17 23:28:27 -0700

    added conditional rendering to studentinfo.js

--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
 export default class StudentInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      student_info: {},
+      accessAllowed: false,
+      firstMount: true
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.history.location.state !== undefined) {
+      this.setState({
+        accessAllowed: true,
+        firstMount: false
+      })
+    }
+  }
+
   render() {
-    return (
-      <div>
-        {this.props.history.location.state}
-      </div>
-    )
+    // if we are allowed access to page and the page is not on the first stage of mounting, display student info
+    if (this.state.accessAllowed && !this.state.firstMount) {
+      return (
+        <div>
+          {this.props.history.location.state}
+        </div>
+      )
+    } else if (!this.state.accessAllowed || this.state.firstMount) {
+      // display error message if access not allowed or it's not first mount
+      return (
+        <div>
+          <h1>Sorry, you have no access to this page</h1>
+        </div>
+      )
+    }
   }
 }
