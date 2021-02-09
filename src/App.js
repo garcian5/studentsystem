31b60a0740e174d66ee381efb8e170cfa3ec003f
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-02-09 10:17:03 -0700

    installed react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // components
 import Home from './components/Home';
