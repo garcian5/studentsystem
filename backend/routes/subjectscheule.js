67244e8a45e1a52debbc1f2d0586b53af395f42b
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-02-09 12:30:22 -0700

    added a router that gets all subject schedules from the database

--- backend/routes/subjectscheule.js
+++ backend/routes/subjectscheule.js
@@ -3,6 +3,11 @@ const validation = require('../helper/validationHelper');
 
 const SubSched = require('../models/subjectScheduleModel');
 
+/**
+ * @route   POST subsched/addsubsched/
+ * @desc    adds a schedule for a subject
+ * @component RegisterStudent.js & UpdateStudent.js 
+ * */
 router.post('/addsubsched', async (req, res) => {
   try {
     const {subject_id, time, day} = req.body;
@@ -13,6 +18,20 @@ router.post('/addsubsched', async (req, res) => {
       subject_id, time, day
     }).save();
     res.json(saveSubSched);
+  } catch (err) { res.status(500).json({error: err.message}); }
+})
+
+/**
+ * @route   GET subsched/allsubscheds
+ * @desc    gets all subject schedules from the database
+ * @component RegisterStudent.js & UpdateStudent.js
+ * */
+router.get('/allsubscheds', async (req, res) => {
+  try {
+    const allSubScheds = await SubSched.find({})
+      .populate({path: 'subject_id', populate: {path: 'instructor_id'}});
+
+    res.json(allSubScheds);
   } catch (err) { res.status(500).json({error: err.message}); }
 })
 
