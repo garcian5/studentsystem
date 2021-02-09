3949f4c775008f8f880b3fe264e08f7e6e9b096f
Nheljee Rose Garcia <garcian5@mymacewan.ca>
2021-02-08 21:09:29 -0700

    updated StudentDirectory.js to fetch data from database

--- src/components/StudentDirectory.js
+++ src/components/StudentDirectory.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 // import database stuff
-import students from '../data/students';
+//import students from '../data/students';
 
 export default class StudentDirectory extends Component {
   constructor() {
     super();
     this.state = {
-      students: students // list of students from database
+      students: [],
+      errorMsg: '',
+      firstMount: true
     }
+    /* this.state = {
+      students: students // list of students from database
+    } */
   }
+
+  componentDidMount () {
+    axios.get('/student/getstudents')
+      .then(res => {
+        this.setState({students: res.data})
+      })
+      .catch(err => { this.setState({ errorMsg: err.response.data.msg }) });
+  }
+
+  componentDidUpdate () {if (this.state.firstMount) this.setState({firstMount: false})}
 
   // go back to departments page
   backBtnClicked = () => {
@@ -26,29 +42,35 @@ export default class StudentDirectory extends Component {
   }
 
   render() {
-    // iterate through the list of students and display their names in the list.
-    const students_lst = this.state.students.map(student => (
-      <ul key={student.id}>
-        <li>
-          <button className='link-style-btn' onClick={() => this.toStudentIfoClicked(student.id)}>{student.lastname}, {student.firstname} {student.middlename}</button>
-        </li>
-      </ul>
-    ))
-    
-    return (
-      <div>
-        <button className='link-style-btn' onClick={this.backBtnClicked}>Back</button>
-        <button className='link-style-btn' onClick={this.backBtnClicked}>Home</button>
-        <h1 className='departments-heading'>ICS Student Directory</h1>
-        <p className='name-lst'>Name List</p>
-        {students_lst.length !== 0 ? students_lst 
-          : 
-          <div>            
-            <p>No Students in this Department. Please register new students here:</p>
-            <button className='link-style-btn' onClick={this.registerClicked}>Register</button>
-          </div>
-        }
-      </div>
-    )
+    if (this.state.firstMount) {
+      return (
+        <div><p>Loading...</p></div>
+      )
+    } else {
+      // iterate through the list of students and display their names in the list.
+      const students_lst = this.state.students.map(student => (
+        <ul key={student._id}>
+          <li>
+            <button className='link-style-btn' onClick={() => this.toStudentIfoClicked(student._id)}>{student.lastname}, {student.firstname} {student.middlename}</button>
+          </li>
+        </ul>
+      ))
+      
+      return (
+        <div>
+          <button className='link-style-btn' onClick={this.backBtnClicked}>Back</button>
+          <button className='link-style-btn' onClick={this.backBtnClicked}>Home</button>
+          <h1 className='departments-heading'>ICS Student Directory</h1>
+          <p className='name-lst'>Name List</p>
+          {students_lst.length !== 0 ? students_lst 
+            : 
+            <div>            
+              <p>No Students in this Department. Please register new students here:</p>
+              <button className='link-style-btn' onClick={this.registerClicked}>Register</button>
+            </div>
+          }
+        </div>
+      )
+    }
   }
 }
